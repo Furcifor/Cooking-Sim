86482fd0dcd534c89c33de1db02d6576c1f5d165
Finn <finn.kinnear@gmail.com>
2019-07-04 13:35:28 +1200

    Displaying window

---
+++ src/main/Launcher.java
@@ -0,0 +1,13 @@
+package main;
+
+import main.display.Display;
+
+public class Launcher {
+	
+	public static void main(String[] args) {
+		
+		new Display("Cooking Simulator!", 300, 300);
+		
+	}
+	
+}
---
+++ src/main/display/Display.java
@@ -0,0 +1,33 @@
+package main.display;
+
+import javax.swing.JFrame;
+
+public class Display {
+	
+	private JFrame frame;
+	
+	private String title;
+	private int width, height;
+	
+	public Display(String title, int width, int height) {
+		
+		this.title = title;
+		this.width = width;
+		this.height = height;
+		
+		initDisplay();
+		
+	}
+	
+	private void initDisplay() {
+		
+		frame = new JFrame(title);
+		frame.setSize(width, height);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+	}
+	
+}
