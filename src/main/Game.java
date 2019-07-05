afff28041f6bddc721245b0a326ba6ad51cb3165
Finn <finn.kinnear@gmail.com>
2019-07-05 15:33:20 +1200

    Image loading

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,97 +1,95 @@
 package main;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 import main.display.Display;
+import main.gfx.ImageLoader;
 
 public class Game implements Runnable {
-	
+
 	private Display display;
 	public int width, height;
 	public String title;
-	
-	private boolean  running = false;
+
+	private boolean running = false;
 	private Thread thread;
-	
+
 	private BufferStrategy bs;
 	private Graphics g;
-	
+
+	private BufferedImage testImage;
+
 	public Game(String title, int width, int height) {
-		
+
 		this.width = width;
 		this.height = height;
 		this.title = title;
-		
+
 	}
-	
+
 	private void init() {
 		display = new Display(title, width, height);
+		testImage = ImageLoader.loadImage("/textures/Gamertag.jpg");
 	}
-	
+
 	private void tick() {
-		
+
+	}
+
+	private void render() {
+
 		bs = display.getCanvas().getBufferStrategy();
-		if(bs == null) {
+		if (bs == null) {
 			display.getCanvas().createBufferStrategy(3);
 			return;
 		}
 		g = bs.getDrawGraphics();
-		
-		//clear screen
+
+		// clear screen
 		g.clearRect(0, 0, width, height);
+
+		// draw here
 		
-		//draw here
-		
-		g.setColor(Color.red);
-		g.fillRect(100, 100, 100, 100);
-		g.setColor(Color.green);
-		g.fillRect(150, 150, 100, 100);
-		g.setColor(Color.blue);
-		g.fillRect(200, 200, 100, 100);
-		
-		
-		//end drawing
-		
+		g.drawImage(testImage, 20, 20, null);
+
+		// end drawing
+
 		bs.show();
 		g.dispose();
-		
-	}
-	
-	private void render() {
-		
+
 	}
 
 	public void run() {
-		
+
 		init();
-		
-		while(running) {
+
+		while (running) {
 			tick();
 			render();
 		}
-		
+
 		stop();
-		
+
 	}
-	
+
 	public synchronized void start() {
-		
+
 		if (running)
 			return;
-		
+
 		running = true;
 		thread = new Thread(this);
 		thread.start();
 	}
-	
+
 	public synchronized void stop() {
-		
-		if(!running)
+
+		if (!running)
 			return;
 		running = false;
-		
+
 		try {
 			thread.join();
 		} catch (InterruptedException e) {
