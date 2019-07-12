c333690bdd74c562d6da028f4bdc672a2ca34ce4
Finn <finn.kinnear@gmail.com>
2019-07-12 16:41:34 +1200

    Improved collision detection

--- src/main/entities/creatures/Player.java
+++ src/main/entities/creatures/Player.java
@@ -43,9 +43,6 @@ public class Player extends Creature{
 	public void render(Graphics g) {
 		g.drawImage(Assets.player, (int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
 		
-		g.setColor(Color.RED);
-		g.fillRect((int) (x + bounds.x - handler.getGameCamera().getxOffset()),
-				(int) (y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
 	}
 
 }
